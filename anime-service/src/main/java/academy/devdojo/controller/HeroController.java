d0461aacacd25e96f74a1187c4e9841d5aa5d976
Renato Lima <56280271+renatolima-dev@users.noreply.github.com>
2025-03-28 13:48:44 -0400

    Video11: using Request Param

--- anime-service/src/main/java/academy/devdojo/controller/HeroController.java
+++ anime-service/src/main/java/academy/devdojo/controller/HeroController.java
@@ -2,18 +2,30 @@ package academy.devdojo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping("v1/heroes")
 public class HeroController {
-    private final List<String> heroes= List.of("Guts","Zoro", "Kakashi","Soku");
+    private final List<String> heroes= List.of("Guts","Zoro", "Kakashi","Goku");
 
     @GetMapping
     public List<String>  getAllHeroes(){
         return heroes;
+    }
+
+    @GetMapping("/filter")
+    public List<String>  getAllHeroesParam(@RequestParam(defaultValue = "") String name){
+        return heroes.stream().filter(hero -> hero.equals(name)).toList();
+    }
+
+    @GetMapping("/filterlist")
+    public List<String>  getAllHeroesParamList(@RequestParam(defaultValue = "") List<String> names){
+        return heroes.stream().filter( names::contains).toList();
     }
 
 }
